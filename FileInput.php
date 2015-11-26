5d118d881db4535003b3dcecf65f9289b1ff9380
Evgeny Vasiliev <comdvas@gmail.com>
2015-11-26 16:01:30 +0300

    fix namespace and custom button text for FileInput widget

--- FileInput.php
+++ FileInput.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace common\widgets;
+namespace comdvas\jasnybootstrap;
 
 use Yii;
 use yii\widgets\InputWidget;
@@ -17,13 +17,28 @@ class FileInput extends InputWidget
      * @var array the HTML attributes for the input tag.
      * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
      */
-    public $options = ['class' => 'form-control'];
+    public $options = [];
 
     /**
      * @var boolean whether to include js.
      * If you load this widget by ajax, don't need to include js.
      */
     public $enableClientScript = true;
+
+    /**
+     * @var text on select file button
+     */
+    public $selectButtonTitle = 'Select file';
+
+    /**
+     * @var text on change file button
+     */
+    public $changeButtonTitle = 'Change';
+
+    /**
+     * @var text on remove file button
+     */
+    public $removeButtonTitle = 'Remove';
 
     /**
      * @inheritdoc
@@ -40,7 +55,6 @@ class FileInput extends InputWidget
             $view = $this->getView();
             JasnyBootstrapAsset::register($view);
         }
-        unset($this->options['class']);
         return implode("\n", [
             Html::beginTag('div', [
                 'class'=>'fileinput fileinput-new input-group',
@@ -51,11 +65,11 @@ class FileInput extends InputWidget
             Html::tag('span', '', ['class'=>'fileinput-filename']),
             Html::endTag('div'),
             Html::beginTag('span', ['class'=>'input-group-addon btn btn-default btn-file']),
-            Html::tag('span', Yii::t('common', 'Select file'), ['class'=>'fileinput-new']),
-            Html::tag('span', Yii::t('common', 'Change'), ['class'=>'fileinput-exists']),
+            Html::tag('span', $this->selectButtonTitle, ['class'=>'fileinput-new']),
+            Html::tag('span', $this->changeButtonTitle, ['class'=>'fileinput-exists']),
             Html::activeFileInput($this->model, $this->attribute, $this->options),
             Html::endTag('span'),
-            Html::a(Yii::t('common', 'Remove'), '#', [
+            Html::a($this->removeButtonTitle, '#', [
                 'class'=>'input-group-addon btn btn-default fileinput-exists', 
                 'data-dismiss'=>'fileinput',
             ]),
